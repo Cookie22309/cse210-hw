591c4a6abf7ca33d50e1bbe00a91a84bc3857b30
Cookie22309 <2bsteele7@gmail.com>
2025-09-18 13:34:13 -0600

    Added a loop that adds to 10

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,7 +6,19 @@ class Program
     {
         Console.WriteLine("Hello World!");
         string x = Console.ReadLine();
-        
+
         Console.WriteLine(x);
+
+        int t = 10;
+
+        int n = 5;
+
+        while (n < t)
+        {
+
+            n += 1;
+            Console.WriteLine(n);
+
+        }
     }
 }
