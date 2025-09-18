fb31d2e479e42578a0fdbc783112379129c6d151
Cookie22309 <2bsteele7@gmail.com>
2025-09-18 13:21:59 -0600

    added read line and print it

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
+        string x = Console.ReadLine();
+        
+        Console.WriteLine(x);
     }
 }
