e92db38019dea83f358b7530d93becb14d42f11d
Cookie22309 <2bsteele7@gmail.com>
2025-09-19 21:05:05 -0600

    Finished prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string grade = Console.ReadLine();
+        int numgrade = int.Parse(grade);
+        string letgrade = "";
+
+        if (numgrade >= 90)
+        {
+            letgrade = "A";
+        }
+        else if (numgrade >= 80 && numgrade < 90)
+        {
+            letgrade = "B";
+        }
+        else if (numgrade >= 70 && numgrade < 80)
+        {
+            letgrade = "C";
+        }
+        else if (numgrade >= 60 && numgrade < 70)
+        {
+            letgrade = "D";
+        }
+        else
+        {
+            letgrade = "F";
+        }
+
+        Console.WriteLine($"You have a {letgrade} in the class!");
+        if (numgrade >= 70)
+        {
+            Console.WriteLine("Congrats! You passed the class! See you next year!!!");
+        }
+        else
+        {
+            Console.WriteLine("Darn, you didnt pass...better luck next time!");
+        }
     }
 }
