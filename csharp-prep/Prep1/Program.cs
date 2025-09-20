a4a1ac07730b8dfc65069ebdf2b0203022531dab
Cookie22309 <2bsteele7@gmail.com>
2025-09-19 20:37:40 -0600

    prep1 2.0

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         Console.Write("What is your First name? ");
         string first = Console.ReadLine();
-        Console.Write("What is your First name? ");
+        Console.Write("What is your Last name? ");
         string last = Console.ReadLine();
         Console.WriteLine($"Your name is {last}, {first} {last}");
 
