90053d1781c92939db1dcc20d074950511eff9b5
Cookie22309 <2bsteele7@gmail.com>
2025-09-25 13:41:05 -0600

    Added function to multiply

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int y = 5;
+        int x = 10;
+        List<int> myList = new List<int>();
+        myList.Add(1);
+        myList.Add(235);
+        myList.Add(345);
+        int count = myList.Count();
+        Console.WriteLine($"The count is {count}");
+        static int multiply(int x, int y)
+        //How to write a comment
+        {
+            int total = x * y;
+            return total;
+        }
+        int answer = multiply(x, y);
+
+        Console.WriteLine($"The answer is {answer}");
+        Console.WriteLine(myList);
     }
 }
