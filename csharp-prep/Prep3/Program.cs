515b023637a29a38f96578f89d191301dfd62a4e
Cookie22309 <2bsteele7@gmail.com>
2025-09-25 20:00:53 -0600

    Added a number guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
+       
+        Random rnd = new Random();
+        int number = rnd.Next(1, 101);
+        int guess = 0;
+        do
+        {
+            Console.WriteLine("Guess what the number is!");
+            string GuessInput = Console.ReadLine();
+            guess = int.Parse(GuessInput);
+            if (guess == number)
+            {
+                Console.WriteLine("Correct!");
+            }
+            if (guess > number)
+            {
+                Console.WriteLine("Lower...");
+            }
+            if (guess < number)
+            {
+                Console.WriteLine("Higher...");
+            }
+        } while (guess != number);
+
+
         int y = 5;
         int x = 10;
         List<int> myList = new List<int>();
