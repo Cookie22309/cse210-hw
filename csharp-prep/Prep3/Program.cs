4d71731e0753ad2f008a184973600b8e4e915195
Cookie22309 <2bsteele7@gmail.com>
2025-09-25 20:22:10 -0600

    Created the ability to add numbers to a list

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,11 +1,9 @@
 using System;
 using Microsoft.VisualBasic;
-
 class Program
 {
     static void Main(string[] args)
     {
-       
         Random rnd = new Random();
         int number = rnd.Next(1, 101);
         int guess = 0;
@@ -27,25 +25,5 @@ class Program
                 Console.WriteLine("Higher...");
             }
         } while (guess != number);
-
-
-        int y = 5;
-        int x = 10;
-        List<int> myList = new List<int>();
-        myList.Add(1);
-        myList.Add(235);
-        myList.Add(345);
-        int count = myList.Count();
-        Console.WriteLine($"The count is {count}");
-        static int multiply(int x, int y)
-        //How to write a comment
-        {
-            int total = x * y;
-            return total;
-        }
-        int answer = multiply(x, y);
-
-        Console.WriteLine($"The answer is {answer}");
-        Console.WriteLine(myList);
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,22 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> myList = new List<int>();
+        int number = 1;
+        do
+        {
+            Console.WriteLine("What is your number?");
+            string input = Console.ReadLine();
+            number = int.Parse(input);
+            myList.Add(number);
+        } while (number != 0);
+        int sum = myList.Sum();
+        double average = myList.Average();
+        int max = myList.Max();
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Average: {average}");
+        Console.WriteLine($"Maximum: {max}");
     }
 }
