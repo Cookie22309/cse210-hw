d2be42b67a43acc229a29cdb2a1fdb9d5dbd0889
Cookie22309 <2bsteele7@gmail.com>
2025-09-25 21:08:40 -0600

    Added fuctions to calculate age

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,52 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        string PromptUserName()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            return name;
+        }
+        int PromptUserNumber()
+        {
+            Console.WriteLine("What is your favorite number?");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            return number;
+        }
+        int birthYear;
+       
+        void PromtUserBirthYear(out int year)
+        {
+            Console.Write("What year were you born? ");
+            string input = Console.ReadLine();
+            while (!int.TryParse(input, out year))
+            {
+                Console.Write("Invalid input. Please enter a valid year: ");
+                input = Console.ReadLine();
+            }
+        }
+        void SquareNumber(int num, string name)
+        {
+            int square = num * num;
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        }
+        void DisplayResult(int year, string name)
+        {
+            int age = 2025 - year;
+            Console.WriteLine($"{name}, you will turn {age} this year."); 
+        }
+        DisplayWelcome();
+        string UserName = PromptUserName();
+        int UserNumber = PromptUserNumber();
+        PromtUserBirthYear(out birthYear);
+        SquareNumber(UserNumber, UserName);
+        DisplayResult(birthYear, UserName);
     }
 }
